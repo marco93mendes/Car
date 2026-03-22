26d31c7a8542e76332ce290591a3437b7442feb5
Marco <marco93mendes@gmail.com>
2026-03-22 16:50:51 -0300

    Take Picture button saving images on "imgbb.com"

--- app/src/main/java/com/example/myapitest/service/RetrofitClient.kt
+++ app/src/main/java/com/example/myapitest/service/RetrofitClient.kt
@@ -1,13 +1,12 @@
 package com.example.myapitest.service
 
-import com.example.myapitest.service.ItemApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    // Address used to access localhost on the Android emulator
     private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val IMGBB_BASE_URL = "https://api.imgbb.com/1/"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
@@ -16,7 +15,18 @@ object RetrofitClient {
             .build()
     }
 
+    private val retrofitImgBB: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(IMGBB_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
     val itemApiService: ItemApiService by lazy {
         retrofit.create(ItemApiService::class.java)
+    }
+
+    val imgBBService: ImgBBService by lazy {
+        retrofitImgBB.create(ImgBBService::class.java)
     }
 }
