2b225f6fcdbf0b59a4e46861d33db451e1941924
Marco <marco93mendes@gmail.com>
2026-03-22 13:37:48 -0300

    Take Picture button working (but saving the image local)

--- app/src/main/java/com/example/myapitest/FormActivity.kt
+++ app/src/main/java/com/example/myapitest/FormActivity.kt
@@ -1,8 +1,20 @@
 package com.example.myapitest
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.content.IntentCompat
 import com.example.myapitest.databinding.ActivityFormBinding
 import com.example.myapitest.model.Item
@@ -16,11 +28,25 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class FormActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFormBinding
     private var item: Item? = null
+    private lateinit var imageUri: Uri
+
+    private val cameraLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            binding.imageUrl.setText("IMAGE OBTAINED!")
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,14 +56,41 @@ class FormActivity : AppCompatActivity() {
         setupView()
     }
 
+    companion object {
+        private const val EXTRA_ITEM = "item"
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 1002
+
+        fun newIntent(context: Context, item: Item? = null): Intent {
+            return Intent(context, FormActivity::class.java).apply {
+                putExtra(EXTRA_ITEM, item)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
     private fun setupView() {
-        item = IntentCompat.getParcelableExtra(intent, "item", Item::class.java)
+        item = IntentCompat.getParcelableExtra(intent, EXTRA_ITEM, Item::class.java)
         val car = item?.value
 
         if (car != null) {
             supportActionBar?.title = "Edit car"
-            //binding.imagePreview.loadUrl(car.imageUrl)
-            binding.image.setText(car.imageUrl)
+            binding.imageUrl.setText(car.imageUrl)
             binding.name.setText(car.name)
             binding.year.setText(car.year)
             binding.licence.setText(car.licence)
@@ -47,6 +100,10 @@ class FormActivity : AppCompatActivity() {
             supportActionBar?.title = "New car"
         }
 
+        binding.takePictureCta.setOnClickListener {
+            takePicture()
+        }
+
         binding.saveCTA.setOnClickListener {
             saveItem()
         }
@@ -54,14 +111,44 @@ class FormActivity : AppCompatActivity() {
         binding.cancelCTA.setOnClickListener { finish() }
     }
 
+    fun takePicture() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            openCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        imageUri = createImageUri()
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        cameraLauncher.launch(intent)
+    }
+
+    private fun createImageUri(): Uri {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        val imageFile = File.createTempFile(imageFileName, ".jpg", storageDir)
+
+        return FileProvider.getUriForFile(this, "com.example.myapitest.fileprovider", imageFile)
+    }
+
     private fun validate(): Boolean {
         var isValid = true
 
-        if (binding.image.text.toString().isBlank()) {
-            binding.imageLayout.error = "Required"
+        if (binding.imageUrl.text.toString().isBlank()) {
+            binding.imageUrlLayout.error = "Required"
             isValid = false
         } else {
-            binding.imageLayout.error = null
+            binding.imageUrlLayout.error = null
         }
 
         if (binding.name.text.toString().isBlank()) {
@@ -94,7 +181,7 @@ class FormActivity : AppCompatActivity() {
         val name = binding.name.text.toString()
         val year = binding.year.text.toString()
         val licence = binding.licence.text.toString()
-        val imageUrl = binding.image.text.toString()
+        val imageUrl = binding.imageUrl.text.toString()
         val lat = binding.lat.text.toString().toDoubleOrNull() ?: 0.0
         val long = binding.lng.text.toString().toDoubleOrNull() ?: 0.0
 
